21efbe289e7ac3cdd2851d8c2e31053c9c4f0b11
yinluobing <896913862@qq.com>
2020-10-10 14:25:25 +0800

    PS-2020.1 <mr-yin@LAPTOP-ABO4M4AS Create markdown.xml, PHP Class.php, ThinkPHP-5.xml, project.default.xml, Default copy.xml, yiiguxing.translation.xml, Default.xml, AngularJS .xml, find.xml, keymap.xml, code.style.schemes.xml, Zen HTML.xml, fastadmin.xml, React.xml, Default.xml, PHP.xml, JavaScript.xml, Zen XSL.xml, editor.xml, Bash.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, web-types-registry.xml, user.xml, PHP Class Doc Comment.php, Zen CSS.xml, Twig.xml, ui.lnf.xml, filetypes.xml, php.xml, xsl.xml, plugin_ui.xml, JavaScript Testing.xml, colors.scheme.xml, html_xml.xml, External Tools.xml, notifications.xml, databaseDrivers.xml, templates.xml, SQL.xml, Angular.xml, laf.xml, path.macros.xml, intentionSettings.xml, wsl.distributions.xml, proxy.settings.xml, HTTP Request.xml, vcs.xml, OnlineSearch_v1_PluginSettings.xml, databaseSettings.xml, usageView.xml, work.xml

---
+++ fileTemplates/includes/PHP Class Doc Comment.php
@@ -0,0 +1,9 @@
+/**
+ * Class ${NAME}
+#if (${NAMESPACE}) * @package ${NAMESPACE}
+#end
+ * @author yinluobing <yinluobing@163.com>
+ * @date ${DATE}
+ * @since 1.0.0
+ * @version 1.0.0
+ */
---
+++ fileTemplates/internal/PHP Class.php
@@ -0,0 +1,12 @@
+<?php
+#parse("PHP Class Doc Comment.php")
+
+#if (${NAMESPACE})
+
+namespace ${NAMESPACE};
+
+#end
+
+class ${NAME} {
+
+}
